77b6e723293967efc43d8dcafbe169a92aa81c6c
Michal Arbet <michal.arbet@ultimum.io>
2024-03-26 11:46:23 +0100

    Extend init-runonce.sh script - create addiditonal resources
    For the upstream CI, we want to use Rally and specifically
    Tempest tests. However, we want to run Tempest through static
    accounts instead of dynamic ones, as it is unnecessary and
    extends the testing time. Ideally, we also do not want to use
    the admin project and admin user, since it has an admin role
    and some tests might not function correctly because of this.
    
    Therefore, it is necessary to add a project, user, and all its
    resources so that we can run the tests. I achieved this by
    modifying the init-runonce.sh script, which now behaves exactly
    as before but has new features if the user calls with parameters.
    
    Outside of CI, this is also useful for the initialization of
    resources outside the admin project during a fresh installation
    of OpenStack.
    
    Change-Id: I5efbb7bc49da0eaba0eba2e909651dc91b96191e

--- tests/init-core-openstack.sh
+++ tests/init-core-openstack.sh
@@ -12,12 +12,15 @@ function init_runonce {
     . ~/openstackclient-venv/bin/activate
 
     echo "Initialising OpenStack resources via init-runonce"
-    KOLLA_DEBUG=1 tools/init-runonce |& gawk '{ print strftime("%F %T"), $0; }' &> /tmp/logs/ansible/init-runonce
+    KOLLA_DEBUG=1 tools/init-runonce ${RUN_ONCE_PARAMS} |& gawk '{ print strftime("%F %T"), $0; }' &> /tmp/logs/ansible/init-runonce
 
     if [[ $IP_VERSION -eq 6 ]]; then
         # NOTE(yoctozepto): In case of IPv6 there is no NAT support in Neutron,
         # so we have to set up native routing. Static routes are the simplest.
         sudo ip route add ${DEMO_NET_CIDR} via ${EXT_NET_DEMO_ROUTER_ADDR}
+        if [[ "${RUN_ONCE_PARAMS}" != "" ]]; then
+            sudo ip route add ${KOLLA_PROJECT_NET_CIDR} via ${EXT_NET_KOLLA_PROJECT_ROUTER_ADDR}
+        fi
     fi
 }
 
